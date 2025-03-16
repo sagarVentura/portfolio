5608ff3022d1408c9aa15227f82502004caa9c59
sagar patil <sagarmp7731@gmail.com>
2025-03-16 17:16:20 +0530

    Add through code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './ui.css';
 import route from './route/Route';
 import { RouterProvider } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+const TRACKING_ID="G-TXWRTK971M"
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    ReactGA.send("pageview"); // Sends a pageview when the app loads
+}, []);
   return (
     <RouterProvider router={route}/>
   );
